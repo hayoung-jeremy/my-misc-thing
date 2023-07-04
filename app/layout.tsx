a32d2e7b9e724ac89badf15e84084232631255b9
Ha young, Kim <74491172+hayoung-jeremy@users.noreply.github.com>
2023-07-04 14:31:26 +0900

    style : Layout transition animation

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { Header } from "@/components/layout";
+import { motion, AnimatePresence } from "framer-motion";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +16,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body className={inter.className}>
         <Header />
-        <main className="flex min-h-screen flex-col items-center justify-center bg-slate-900">{children}</main>
+        {children}
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,16 @@ export default function Home() {
   const [hoveredIdx, setHoveredIdx] = useState(-1);
 
   return (
-    <>
+    <motion.main
+      layout
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{
+        opacity: { ease: "linear" },
+        layout: { duration: 0.3 },
+      }}
+      className="flex min-h-screen flex-col items-center justify-center bg-slate-900"
+    >
       <div className="w-full max-w-[400px]">
         <ul className="h-[400px] mask-container overflow-x-hidden overflow-y-auto py-[40px] flex flex-col">
           {Array.from({ length: 40 }, (_, index) => (
@@ -34,6 +43,6 @@ export default function Home() {
           <p>test item {index + 1}</p>
         </div>
       ))}
-    </>
+    </motion.main>
   );
 }
