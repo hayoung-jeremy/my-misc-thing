7a4bc3c5d8a47d8ccab8d76ae876fa16bb4ac8df
Ha young, Kim <74491172+hayoung-jeremy@users.noreply.github.com>
2023-07-06 17:56:57 +0900

    Update Article.tsx

--- src/components/project/Article.tsx
+++ src/components/project/Article.tsx
@@ -1,7 +1,8 @@
-import { Dispatch, SetStateAction, useRef } from "react";
+import { Dispatch, SetStateAction, useEffect, useRef } from "react";
 import { motion } from "framer-motion";
 
 import { cls } from "@/utils";
+import { Close } from "../icons";
 
 interface Props {
   listIdx: number;
@@ -19,11 +20,15 @@ const Article = ({ listIdx, title, desc, thumbnailImg, currentOpenArticleIdx, se
   const openSpring = { type: "spring", stiffness: 200, damping: 30 };
   const closeSpring = { type: "spring", stiffness: 300, damping: 35 };
 
+  useEffect(() => {
+    console.log("currentOpenArticleIdx : ", currentOpenArticleIdx);
+  }, [currentOpenArticleIdx]);
+
   return (
     <motion.li
       ref={containerRef}
       transition={{ duration: 0.3 }}
-      className="relative flex-[1_1_auto] md:flex-[1_1_40%] xl:flex-[1_1_30%] md:max-w-[48.5%] xl:max-w-[38.5%] h-[112vw] md:h-[400px]"
+      className="relative w-full flex-[1_1_auto] md:flex-[1_1_40%] xl:flex-[1_1_30%] md:max-w-[48.5%] xl:max-w-[38.5%] h-[112vw] md:h-[400px]"
     >
       {/* Overlay */}
       <motion.div
@@ -32,7 +37,7 @@ const Article = ({ listIdx, title, desc, thumbnailImg, currentOpenArticleIdx, se
         animate={{ opacity: currentOpenArticleIdx === listIdx ? 1 : 0 }}
         transition={{ duration: 0.2 }}
         style={{ pointerEvents: currentOpenArticleIdx === listIdx ? "auto" : "none" }}
-        className="[Overlay] fixed z-20 bg-black/40 top-0 left-0 w-screen h-screen"
+        className="[Overlay] fixed z-20 bg-black/60 top-0 left-0 w-screen h-screen"
       />
 
       {/* Card content */}
@@ -46,7 +51,6 @@ const Article = ({ listIdx, title, desc, thumbnailImg, currentOpenArticleIdx, se
             : "relative block w-full h-full"
         )}
       >
-        <button className="absolute top-5 right-5">X</button>
         <motion.div
           ref={cardRef}
           layout
@@ -54,16 +58,33 @@ const Article = ({ listIdx, title, desc, thumbnailImg, currentOpenArticleIdx, se
           className={cls(
             "[card-content]",
             "relative",
-            "overflow-hidden m-auto",
+            "overflow-hidden",
+            "w-full",
             currentOpenArticleIdx === listIdx
               ? "h-auto max-w-[700px] bg-blue-900 md:rounded-[20px]"
-              : " bg-slate-900 w-full h-full rounded-[20px]"
+              : "bg-slate-900 h-full rounded-[20px]"
           )}
         >
-          <div className="w-full aspect-square md:h-[320px] bg-white/5" />
-          <div className="px-4 py-3">
+          <div className="w-full aspect-square md:aspect-auto md:h-[320px] bg-white/5 relative">
+            {currentOpenArticleIdx === listIdx && (
+              <motion.button
+                onClick={e => {
+                  e.stopPropagation();
+                  setCurrentOpenArticleIdx(-10);
+                }}
+                layout
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.2, delay: 0.5 }}
+                className="absolute top-5 right-5 w-10 h-10 flex items-center justify-center"
+              >
+                <Close />
+              </motion.button>
+            )}
+          </div>
+          <div className={cls("py-3", currentOpenArticleIdx === listIdx ? "px-7" : "px-4")}>
             <p className="font-AppleSDGothicNeoM text-[20px]">{title}</p>
-            <p className="mb-4">sub title</p>
+            <p className="mb-4">{desc}</p>
             <p>
               {
                 "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
