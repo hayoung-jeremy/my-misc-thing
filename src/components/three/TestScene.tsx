8a92906606cd7149a4f53876a261d0741c1f96f3
Ha young, Kim <74491172+hayoung-jeremy@users.noreply.github.com>
2023-07-17 15:45:27 +0900

    chore : clean up

--- src/components/three/TestScene.tsx
+++ src/components/three/TestScene.tsx
@@ -4,33 +4,14 @@
 */
 
 import useSpline from "@splinetool/r3f-spline";
-import { OrbitControls, OrthographicCamera, PerspectiveCamera } from "@react-three/drei";
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import * as THREE from "three";
 
-import { Lensflare, LensflareElement } from "three/examples/jsm/objects/Lensflare";
-import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader";
-import { Reflector } from "three/examples/jsm/objects/Reflector";
 import { useFrame, useThree } from "@react-three/fiber";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
 export default function TestScene({ ...props }) {
-  const { nodes, materials } = useSpline("https://prod.spline.design/JIhVySyKhvZzzzn7/scene.splinecode");
   const torusKnotRef = useRef<any>(null);
-
-  const { scene } = useThree();
-
-  const loader = new THREE.TextureLoader();
-  const texture0 = loader.load("/textures/lensflare0.png");
-  const texture3 = loader.load("/textures/lensflare3.png");
-
-  const lensflare = new Lensflare();
-  lensflare.position.set(0, 5, -5);
-  lensflare.addElement(new LensflareElement(texture0, 700, 0));
-  lensflare.addElement(new LensflareElement(texture3, 60, 0.6));
-  lensflare.addElement(new LensflareElement(texture3, 70, 0.7));
-  lensflare.addElement(new LensflareElement(texture3, 120, 0.9));
-  lensflare.addElement(new LensflareElement(texture3, 70, 1));
-  scene.add(lensflare);
 
   useFrame(({ clock }) => {
     if (torusKnotRef.current) torusKnotRef.current.rotation.x = clock.elapsedTime * 0.4;
@@ -38,7 +19,7 @@ export default function TestScene({ ...props }) {
 
   return (
     <>
-      {/* <color attach="background" args={["#74757a"]} /> */}
+    
       <group {...props} dispose={null}>
         <directionalLight
           name="Directional Light"
@@ -64,58 +45,7 @@ export default function TestScene({ ...props }) {
             side={THREE.DoubleSide}
           />
         </mesh>
-        {/* <group name="Group">
-          <group name="Rectangle">
-            <mesh
-              name="Rectangle1"
-              geometry={nodes.Rectangle1.geometry}
-              material={materials[""]}
-              position={[0, 0, 15]}
-            />
-            <mesh
-              name="Rectangle2"
-              geometry={nodes.Rectangle2.geometry}
-              material={materials[""]}
-              position={[0, 0, -5]}
-              scale={0.8}
-            />
-            <mesh
-              name="Rectangle3"
-              geometry={nodes.Rectangle3.geometry}
-              material={materials[""]}
-              position={[0, 0, -10]}
-              scale={0.6}
-            />
-            <mesh
-              name="Rectangle4"
-              geometry={nodes.Rectangle4.geometry}
-              material={materials[""]}
-              position={[0, 0, -15]}
-              scale={0.9}
-            />
-            <mesh
-              name="Rectangle5"
-              geometry={nodes.Rectangle5.geometry}
-              material={materials[""]}
-              position={[0, 0, 5]}
-              scale={0.8}
-            />
-            <mesh
-              name="Rectangle6"
-              geometry={nodes.Rectangle6.geometry}
-              material={materials[""]}
-              position={[0, 0, 10]}
-            />
-          </group>
-        </group> */}
-        {/* <OrthographicCamera
-          name="1"
-          // makeDefault={true}
-          // zoom={0.49}
-          far={100000}
-          near={-100000}
-          position={[99.62, 84.38, 1000]}
-        /> */}
+
         <hemisphereLight name="Default Ambient Light" intensity={0.75} color="#eaeaea" />
         <gridHelper />
         <axesHelper />
