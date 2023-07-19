1c9c6ed39750bcbcec55dcde93d80a172dae6a40
Ha young, Kim <74491172+hayoung-jeremy@users.noreply.github.com>
2023-07-19 18:38:00 +0900

    feat : import and use exported animation from `theatre.js`

--- src/components/three/TheatreScene.tsx
+++ src/components/three/TheatreScene.tsx
@@ -1,24 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { getProject } from "@theatre/core";
 import studio from "@theatre/studio";
 import extension from "@theatre/r3f/dist/extension";
-import { editable as e, SheetProvider } from "@theatre/r3f";
+import { editable as e, SheetProvider, PerspectiveCamera } from "@theatre/r3f";
+import { OrbitControls } from "@react-three/drei";
+import demoProjectState from "@/animations/DemoProject.theatre-project-state.json";
 
-const demoSheet = getProject("Demo Project").sheet("Demo Sheet");
+const demoSheet = getProject("Demo Project", { state: demoProjectState }).sheet("Demo Sheet");
 if (process.env.NODE_ENV === "development") {
   studio.initialize();
   studio.extend(extension);
 }
 
 const TheatreScene = () => {
+  useEffect(() => {
+    demoSheet.project.ready.then(() => demoSheet.sequence.play({ iterationCount: 2, range: [0, 1] }));
+  }, []);
+
   return (
     <SheetProvider sheet={demoSheet}>
+      <PerspectiveCamera
+        theatreKey="Camera"
+        makeDefault
+        position={[5, 5, -5]}
+        fov={75}
+        attachArray={undefined}
+        attachObject={undefined}
+        attachFns={undefined}
+        lookAt={[0, 0, 0]}
+      />
       <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      <mesh>
+      <e.pointLight theatreKey="pointLight" position={[10, 10, 10]} />
+      <e.mesh theatreKey="box">
         <boxGeometry args={[1, 1, 1]} />
         <meshStandardMaterial color="orange" />
-      </mesh>
+      </e.mesh>
+      <OrbitControls />
     </SheetProvider>
   );
 };
