ff1f9ab4c94b6dca824d296b0f4f7127ba84436b
Ha young, Kim <74491172+hayoung-jeremy@users.noreply.github.com>
2023-08-18 14:02:54 +0900

    feat : display name for each PortalCards' character

--- src/components/three/PortalScene.tsx
+++ src/components/three/PortalScene.tsx
@@ -1,34 +1,32 @@
 import React, { Suspense } from "react";
 import * as THREE from "three";
-import {
-  Environment,
-  MeshPortalMaterial,
-  OrbitControls,
-  PerspectiveCamera,
-  RoundedBox,
-  useTexture,
-} from "@react-three/drei";
+import { Environment, MeshPortalMaterial, OrbitControls, RoundedBox, Text, useTexture } from "@react-three/drei";
 import { Alpaca, Deer, Husky } from "./models";
-import { GroupProps } from "@react-three/fiber";
 
 const PortalScene = () => {
-  const map = useTexture("textures/digital_painting_dark_city_with_many_stars_and_lig.jpg");
-
   return (
     <Suspense fallback={null}>
       <ambientLight intensity={0.5} />
       <OrbitControls />
       <PortalCard
+        name="Husky"
+        color="#bbb1a5"
         texture="textures/digital_painting_dark_city_with_many_stars_and_lig.jpg"
         position-x={-2.5}
         rotation-y={Math.PI / 8}
       >
         <Husky scale={0.5} position-y={-1} />
       </PortalCard>
-      <PortalCard texture="textures/digital_painting_deep_forest_with_flowers.jpg">
+      <PortalCard name="Alpaca" color="#ae855a" texture="textures/digital_painting_deep_forest_with_flowers.jpg">
         <Alpaca scale={0.4} position-y={-1} />
       </PortalCard>
-      <PortalCard texture="textures/digital_painting_water_with_flowers.jpg" position-x={2.5} rotation-y={-Math.PI / 8}>
+      <PortalCard
+        name="Deer"
+        color="#d49971"
+        texture="textures/digital_painting_water_with_flowers.jpg"
+        position-x={2.5}
+        rotation-y={-Math.PI / 8}
+      >
         <Deer scale={0.5} position-y={-1} />
       </PortalCard>
     </Suspense>
@@ -38,12 +36,18 @@ const PortalScene = () => {
 interface PortalCardProps {
   children: React.ReactNode;
   texture: string;
+  name: string;
+  color: string;
 }
 
-const PortalCard = ({ children, texture, ...props }: JSX.IntrinsicElements["group"] & PortalCardProps) => {
+const PortalCard = ({ children, texture, name, color, ...props }: JSX.IntrinsicElements["group"] & PortalCardProps) => {
   const map = useTexture(texture);
   return (
     <group {...props}>
+      <Text font="fonts/AppleSDGothicNeoR.ttf" fontSize={0.3} position={[-0.42, -1.4, 0.051]} anchorY={"bottom"}>
+        {name}
+        <meshBasicMaterial color={color} toneMapped={false} />
+      </Text>
       <RoundedBox args={[2, 3, 0.1]}>
         <MeshPortalMaterial>
           <Environment preset="sunset" />
