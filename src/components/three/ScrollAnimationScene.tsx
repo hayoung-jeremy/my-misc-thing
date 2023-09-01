32d523f6ef7557d72e37bea2d4bff7983ca20dab
Ha young, Kim <74491172+hayoung-jeremy@users.noreply.github.com>
2023-09-01 16:16:38 +0900

    feat : 초기 화면 animation using gsap

--- src/components/three/ScrollAnimationScene.tsx
+++ src/components/three/ScrollAnimationScene.tsx
@@ -1,29 +1,62 @@
-import { Box, Environment, OrbitControls, ScrollControls, Text } from "@react-three/drei";
-import React from "react";
+import React, { useEffect, useLayoutEffect, useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+import { Box, Environment, OrbitControls, ScrollControls, Text, useScroll } from "@react-three/drei";
 import { degToRad } from "three/src/math/MathUtils";
+import { gsap } from "gsap";
 
 const ScrollAnimationScene = () => {
+  const SceneRef = useRef<any>(null);
+  const timeline = useRef<gsap.core.Timeline>();
+
+  const scroll = useScroll();
+
+  useFrame(() => {
+    if (!timeline.current || scroll === null) return;
+
+    timeline.current.seek(scroll.offset * timeline.current.duration());
+  });
+
+  useLayoutEffect(() => {
+    timeline.current = gsap.timeline();
+
+    timeline.current.to(
+      SceneRef.current.position,
+      {
+        duration: 2,
+        y: 0,
+      },
+      0
+    );
+  }, []);
+
+  useEffect(() => {
+    console.log("timeline : ", timeline.current);
+    console.log("scroll : ", scroll);
+  }, [scroll]);
+
   return (
     <>
       <ScrollControls pages={3} damping={0.25}>
-        <Box position={[0, 0, 0]}>
-          <meshStandardMaterial color={"#ff0000"} />
-          <Text position={[0, 0, 0.51]} scale={0.5}>
-            1
-          </Text>
-        </Box>
-        <Box position={[0, -1, -1]}>
-          <meshStandardMaterial color={"#dbbc64"} metalness={0.78} roughness={0.2} envMapIntensity={0.8} />
-          <Text position={[-0.51, 0, 0]} scale={0.5} rotation-y={-degToRad(90)}>
-            2
-          </Text>
-        </Box>
-        <Box position={[1, -2, -1]}>
-          <meshStandardMaterial color={"#00ff00"} roughness={1} metalness={0} />
-          <Text position={[0, 0, -0.51]} scale={0.5} rotation-y={degToRad(180)}>
-            3
-          </Text>
-        </Box>
+        <group ref={SceneRef} position={[0, -2, 0]}>
+          <Box position={[0, 0, 0]}>
+            <meshStandardMaterial color={"#ff0000"} />
+            <Text position={[0, 0, 0.51]} scale={0.5}>
+              1
+            </Text>
+          </Box>
+          <Box position={[0, -1, -1]}>
+            <meshStandardMaterial color={"#dbbc64"} metalness={0.78} roughness={0.2} envMapIntensity={0.8} />
+            <Text position={[-0.51, 0, 0]} scale={0.5} rotation-y={-degToRad(90)}>
+              2
+            </Text>
+          </Box>
+          <Box position={[1, -2, -1]}>
+            <meshStandardMaterial color={"#00ff00"} roughness={1} metalness={0} />
+            <Text position={[0, 0, -0.51]} scale={0.5} rotation-y={degToRad(180)}>
+              3
+            </Text>
+          </Box>
+        </group>
       </ScrollControls>
 
       <Environment preset="sunset" />
